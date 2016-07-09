e02c122dea9c4c9843e8889f40022bd74f5ddc9e
max <flexagon89@gmail.com>
2016-07-09 23:19:15 +0300

    fixed gumhint code comments

--- Task *3/js/jquery.gumhint-1.0.0.js
+++ Task *3/js/jquery.gumhint-1.0.0.js
@@ -1,71 +1,65 @@
 ;(function($){
 	jQuery.fn.gumhint = function(options){
-		//Options
-		var options = $.extend({
-			'delay': '1000',
-			'animate': true
+		//Настройки
+		var options = $.extend({ // Объединяем настройки в один объект
+			'delay': '1000', // Выставляем задержку
+			'animate': true // Выставляем анимацию
 		}, options );
 		var timeOut;
 
-		$(this).on('mouseenter', function() {
-				var $dataElement = $(this);
+		$(this).on('mouseenter', function() { // Создаем нашу основную функцию для подсказок
 
-				timeOut = setTimeout(function(){
-			if (!$dataElement.is(":hover")) {
-				return false;
-			}
+			var $dataElement = $(this); // Помещаем наш инициализирующий класс в переменную
 
-			// Check exist data-attribute data-gumhint
-			if ($dataElement.data("gumhint")){
-				// gumhint text
-				var gumhintText = $dataElement.data("gumhint");
-
-				// add gumhint in DOM
-				$("body").append("<span class=gumhint style='display: none'>"+ gumhintText +"</span>");
-				//check animate option
-				if(options.animate){
-					$('.gumhint').fadeIn(300);
-				} else {
-					$('.gumhint').show();
+			timeOut = setTimeout(function(){
+				if (!$dataElement.is(":hover")) { // Если мы наводим мышку не на наш объект
+					return false; // То отменяем все действия по умолчанию
 				}
 
-				// set position hit
-				$(".gumhint").each(function(){
-					gumhintPosition($dataElement);
-				});
-			}
+				if ($dataElement.data("gumhint")){ // Проверяем наличие требуемого дата аттрибута
+
+					var gumhintText = $dataElement.data("gumhint"); // Берем текст из дата аттрибута, и помещаем его в переменную
+
+					$("body").append("<span class=gumhint style='display: none'>"+ gumhintText +"</span>"); // Добавляем блок с всплывающей подсказкой
+
+					if(options.animate){ // Если включена анимация
+						$('.gumhint').fadeIn(300); // То плавно показываем нашу подсказку (по умолчанию 0.3 секунды)
+					} else { // Если же анимация выключена
+						$('.gumhint').show(); // Просто показываем блок
+					}
+
+					$(".gumhint").each(function(){ // Выполняем функцию для проверки положения подсказки
+						gumhintPosition($dataElement);
+					});
+				}
 			}, options.delay);
 
-		}).on('mouseleave', function() {
-			clearTimeout(timeOut);
-			$(".gumhint").remove();
+		}).on('mouseleave', function() { // Когда убираем мышку с объекта
+			clearTimeout(timeOut); // Отменяем выполнение кода
+			$(".gumhint").remove(); // И убираем класс у нашей подсказки
 		});
 
 	};
 
-	function gumhintPosition(el) {
+	function gumhintPosition(el) { // Функция для проверки положения подсказки
 		var gumhint = $(".gumhint"),
-				widthGumHint = gumhint.outerWidth(),
-				heightGumHint = gumhint.outerHeight(),
-				leftEl = el.offset().left,
+				widthGumHint = gumhint.outerWidth(), // Ширина блока подсказки
+				heightGumHint = gumhint.outerHeight(), // Высота блока подсказки
+				leftEl = el.offset().left, // Положение родительского элемента подсказки относительно окна
 				topEl = el.offset().top,
 				ElHeight = el.outerHeight(), //height hover element
 				ElWidth = el.outerWidth(); //width hover element
 
-		var posElLeft = el.offset().left,
-				posElTop = el.offset().top;
+		gumhint.css({"top": topEl+ElHeight+5,"left": leftEl+ElWidth/2, "margin-left": -widthGumHint/2}); // Высчитываем отступы подсказки от родительского элемента
 
-	gumhint.css({"top": topEl+ElHeight+5,"left": leftEl+ElWidth/2, "margin-left": -widthGumHint/2});
-
-	// if hint out of screen border's
-		var rightScreenBorder = $(window).width() - (gumhint.offset().left + widthGumHint),
+		var rightScreenBorder = $(window).width() - (gumhint.offset().left + widthGumHint), // Высчитываем отступы подсказки относительно окна
 				leftScreenBorder = gumhint.offset().left;
 
-	if(leftScreenBorder < 0){
-		gumhint.css({"left": leftEl+ElWidth/1.7});
+		if(leftScreenBorder < 0){ // Если отступ от левого края окна до подсказки меньше 0
+			gumhint.css({"left": leftEl+ElWidth/1.7}); // То делаем новый отступ
+		}
+		if(rightScreenBorder < 0){ // Если отступ от правого края окна до подсказки меньше 0
+			gumhint.css({"right": leftEl+ElWidth/1.7}); // ну вы поняли
+		}
 	}
-	if(rightScreenBorder < 0){
-		gumhint.css({"right": leftEl+ElWidth/1.7});
-	}
-	}
-})(jQuery);
+})(jQuery); // Конец плагина
